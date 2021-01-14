6746bdb0f2db2028bdb3df573b657a2be6284649
mack millar <mackmillar@gmail.com>
2021-01-14 16:32:56 +0000

    end of day

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
-<template>
+<template lang="html">
   <div id="app">
     <h1>Brewdog Beers</h1>
     <beer-list :beerList="beerList"></beer-list>
     <beer-details :beer="selectedBeer"></beer-details>
+    <div v-if="favourites.length > 0">
+      <h2>Favourites</h2>
+      <ul>
+        <li v-for="favourite in favourites">{{favourite.name}} <button v-on:click="removeFromFavourites(favourite)">Remove</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -21,22 +27,45 @@ export default {
     return {
       beerList: [],
       selectedBeer: null,
-      favourites: []
+      favourites: [],
+      page2: [],
+      newList: []
     }
   },
   mounted(){
-    fetch('https://api.punkapi.com/v2/beers')
-    .then(res => res.json())
-    .then(beerList => this.beerList = beerList)
-
+    this.getBeers()
+    
     eventBus.$on('beer-selected', (beer) => {
       this.selectedBeer = beer
     })
     eventBus.$on('favourite', (beer) =>{
       this.favourites.push(beer)
     })
-  }
-}
+    eventBus.$emit('favouitesList', (this.favourites))
+  },
+  methods: {
+    removeFromFavourites(favourite){
+      const newList = this.favourites.filter(beer => favourite != beer)
+      this.favourites = newList
+    },
+    getBeers: function () {
+      const page1 = fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
+      .then(res => res.json())
+      .then(beerList => this.beerList = beerList)
+
+      const page2 = fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
+      .then(res => res.json())
+      .then(page2 => this.page2 = page2)
+
+      Promise.all([page1, page2])
+      .then(this.beerList.forEach(beer => this.newList.push(beer)))
+    },
+    
+    }
+    }
+  
+
+
 </script>
 
 <style>
--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -2,6 +2,18 @@
   <div v-if="beer">
       <h2>{{beer.name}}</h2>
       <p>{{beer.description}}</p>
+      <div>
+      <h3>Ingredients</h3>
+        <p>Malt</p>
+      <ul>
+      <li v-for="subIngredient in beer.ingredients.malt">{{subIngredient.name}}</li>
+      </ul>
+      <p>Hops</p>
+      <li v-for="subIngredient in beer.ingredients.hops" v-if="onlyOne(subIngredient, beer.ingredients.hops)">{{subIngredient.name}}</li>
+      </ul>
+      <p>Yeast</p>
+      <li>{{beer.ingredients.yeast}}</li>
+      </div>
       <img :src="beer.image_url" alt="">
       <button v-on:click="addToFavourites">Favourite</button>
   </div>
@@ -13,12 +25,41 @@ import { eventBus } from '../main.js'
 export default {
     name: 'beer-details',
     props: ['beer'],
+    data(){
+    return {
+        favouritesList: [],
+    }
+    },
     methods: {
         addToFavourites(){
-            eventBus.$emit("favourite", this.beer)
+            eventBus.$emit("favourite", this.beer),
+            this.favouritesList.push(this.beer)
+        },
+        notFavourited(){
+            for (beer in this.favouritesList) {
+                if (beer.name === this.beer.name){
+                    return false
+                }
+                return true
+            }
+        },
+        onlyOne(ingredient, array){
+            let newList = []
+            newList = array.filter(item => item.name === ingredient.name)
+            console.log('new list', newList)
+            if (newList.length < 2){
+                return true
+            }
         }
     }
-}
+    }
+    // mounted(){
+    //     eventBus.$on('favouritesList', (favouriteList) =>{
+    //         this.favouritesList = favouritesList
+    //     })
+    // },
+
+
 </script>
 
 <style>
